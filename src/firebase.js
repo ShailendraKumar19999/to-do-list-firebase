598e46d509c53fae59c9643314941e78a1ac53f8
Shailendra <42029400+ShailendraKumar19999@users.noreply.github.com>
2022-06-11 20:30:53 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import {getFirestore} from 'firebase/firestore'
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyD058nKqJiHVAyMvEpIGI_gAh3NycMk3SQ',
-  authDomain: 'todo-crud-9cbbb.firebaseapp.com',
-  projectId: 'todo-crud-9cbbb',
-  storageBucket: 'todo-crud-9cbbb.appspot.com',
-  messagingSenderId: '338758423795',
-  appId: '1:338758423795:web:ee667413ac45af216bd386',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 };
 
 // Initialize Firebase
